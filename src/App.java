825c36155ab80ddab1877cae53c8859e8ff35d90
Isa Giongo <isagiongo@gmail.com>
2019-11-24 12:12:42 -0300

    Adiciona mais stream de teste

--- src/App.java
+++ src/App.java
@@ -1,11 +1,16 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class App {
 
     public static void main(String[] args) {
+
+        IntStream.range(1,10).skip(5).forEach(System.out::println);
+
         List<Person> peopleList = getPeople();
 
         // Imperative approach
@@ -20,6 +25,8 @@ public class App {
         females.forEach(System.out::println);*/
 
         //Declarative
+        System.out.println();
+        System.out.println("Filtra por gender female, ordenando por idade e depois por nome e printa essa lista");
         List<Person> females = peopleList.stream()
                 .filter(p -> p.getGender().equals(Gender.FEMALE))
                 .sorted(Comparator.comparing(Person::getAge)
@@ -29,11 +36,15 @@ public class App {
         females.forEach(System.out::println);
 
         //AnyMatch
+        System.out.println();
+        System.out.println("Printa true se houver qualquer item com age menor que 20");
         Boolean hasTeenager = peopleList.stream()
                 .anyMatch(p -> p.getAge() < 20);
         System.out.println(hasTeenager);
 
-        //Group
+        //Group for gender
+        System.out.println();
+        System.out.println("Agrupa por gender e printa separadamente");
         Map<Gender, List<Person>> groupByGender = peopleList.stream()
                 .collect(Collectors.groupingBy(Person::getGender));
 
@@ -43,12 +54,41 @@ public class App {
             System.out.println();
         });
 
-        //Max
+        //mais velha por genero female e printar só nome
+        System.out.println();
+        System.out.println("Filtro por gender, pegando a pessoa mais velha e printar só nome");
         Optional<String> oldestFemaleAge = peopleList.stream()
                 .filter(person -> person.getGender().equals(Gender.FEMALE))
                 .max(Comparator.comparing(Person::getAge))
                 .map(Person::getName);
         oldestFemaleAge.ifPresent(System.out::println);
+
+        //mais novo
+        System.out.println();
+        System.out.println("Orderna por idade, pega a primeira e printa");
+        peopleList.stream()
+                .sorted(Comparator.comparing(Person::getAge))
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        //mais novo female
+        System.out.println();
+        System.out.println("Filtra por gender, ordena por idade reversa, pega a primeira e printa");
+        peopleList.stream()
+                .filter(p -> p.getGender().equals(Gender.FEMALE))
+                .sorted(Comparator.comparing(Person::getAge)
+                        .reversed())
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        //filtrar por nomes começados por A ordenando por idade
+        System.out.println();
+        System.out.println("Filtra por nomes começados por A do gender female e ordena por idade");
+        peopleList.stream()
+                .filter(p -> p.getName().startsWith("A"))
+                .filter(p -> p.getGender().equals(Gender.FEMALE))
+                .sorted(Comparator.comparing(Person::getAge))
+                .forEach(System.out::println);
     }
 
     private static List<Person> getPeople() {
